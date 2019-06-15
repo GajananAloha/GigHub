c6a75a94dd6960fa79e3d6f976c604ef4fd58aa1
GajananAloha <gpchavan050@gmail.com>
2019-06-15 18:20:01 +0530

    Added Attend Gig functionality

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,39 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public IHttpActionResult Get()
+        {
+            return Ok("Testing");
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = new Attendance()
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest();
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
